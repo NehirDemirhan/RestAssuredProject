41788a9ce66e67002ffd8ea51fc4cd17d3d5b2b3
ysrdrl <yasardereli6100@gmail.com>
2022-10-14 16:46:38 +0300

    bank accounts

---
+++ src/test/java/CampusProject/BankAccount/RandomInformation.java
@@ -0,0 +1,18 @@
+package CampusProject.BankAccount;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class RandomInformation {
+
+    public static String randomName() {
+
+        return RandomStringUtils.randomAlphabetic(5);
+    }
+
+    public static String randomNumber(int basamak) {
+        return RandomStringUtils.randomNumeric(basamak);
+    }
+
+
+
+}
